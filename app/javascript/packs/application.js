751e2ba77321e364f489c007bfeb768fd1ac6146
auliafaizahr <zahaulia@gmail.com>
2021-02-19 22:14:17 +0700

    WIP Index homepage comments on post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import '../stylesheets/application.scss'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//bootstrap
+require("bootstrap")
+require("@fortawesome/fontawesome-free/scss/fontawesome.scss")
+require("@fortawesome/fontawesome-free/js/all.js")
+// require("../stylesheets/application.scss");
 
+// import './alert'
+
+window.jQuery = $;
+window.$ = $;
+
+setTimeout(function() {
+  $('.alert').slideUp();
+}, 1000);
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
