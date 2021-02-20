5bd169118e9ddc61337559c82203984384243e8a
auliafaizahr <zahaulia@gmail.com>
2021-02-20 21:13:31 +0700

    Styling template comment done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,19 +16,10 @@ require("channels")
 require("bootstrap")
 require("@fortawesome/fontawesome-free/scss/fontawesome.scss")
 require("@fortawesome/fontawesome-free/js/all.js")
-// require("../stylesheets/application.scss");
 
-// import './alert'
+import './home'
 
 window.jQuery = $;
 window.$ = $;
 
-setTimeout(function() {
-  $('.alert').slideUp();
-}, 1000);
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
---
+++ app/javascript/packs/home.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('input[type="submit"], input[type="button"], button').attr("disabled", true);
+
+  $('input[type="text"],textarea').on("keyup", function () {
+    var textarea_value = $('input[type="text"],textarea').val();
+    // var text_value = $('input[name="textField"]').val();
+
+    if (textarea_value != "") {
+      $('input[type="submit"], input[type="button"], button').attr("disabled", false);
+
+      // $('input[type="submit"]').attr("disabled", false);
+    } else {
+      $('input[type="submit"], input[type="button"], button').attr("disabled", true);
+    }
+  });
+});
