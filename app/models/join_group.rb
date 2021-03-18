bc51efa1f48e4ae9854d3386b630117eb59490e1
auliafaizahr <zahaulia@gmail.com>
2021-03-19 00:38:12 +0700

    WIP Avatar per group

--- app/models/join_group.rb
+++ app/models/join_group.rb
@@ -2,6 +2,8 @@ class JoinGroup < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
+  has_one_attached :avatar, dependent: :destroy
+
   def unique?(username, group)
     nickname = username.scan(/\w+/).to_sentence
     all_username = []
